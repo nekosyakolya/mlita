4e67c9dddf3182664a10fadf3e732fc5d73815c8
nekosyakolya <greirex@mail.ru>
2017-04-29 19:10:03 +0300

    вынесение по функциям

--- labs/lw-13/lw-13/lw-13.cpp
+++ labs/lw-13/lw-13/lw-13.cpp
@@ -4,16 +4,23 @@
 #include <iostream>
 #include <string>
 
-//¨????????????
+struct Position
+{
+	size_t row = 1;
+	size_t column = 0;
+};
+
 void ToLowerCase(char & symbol)
 {
-	if ((symbol <= 'ß' && symbol >= 'À') || symbol <= 'Z' && symbol >= 'A')
+	if ((symbol <= 'ß' && symbol >= 'À') || (symbol <= 'Z' && symbol >= 'A' ))
 	{
 		symbol += 32;
 	}
+	if (symbol == '¨')
+	{
+		symbol += 16;
+	}
 }
-
-
 
 std::multimap <char, size_t> GetBase(std::string & sample)
 {
@@ -26,65 +33,81 @@ std::multimap <char, size_t> GetBase(std::string & sample)
 	return tmp;
 }
 
+size_t GetNumberIdenticalSymbols(size_t lastIndex, size_t &j)
+{
+	size_t result = 0;
+	if (j < lastIndex)
+	{
+		result = lastIndex - j;
+		j = lastIndex;
+	}
+	return result;
+}
 
-bool isFindString(std::string & sample, std::string & text, std::ofstream &output)
+size_t GetNewIndex(size_t &j, size_t size, const std::multimap <char, size_t>& base, char symbol)
+{
+	size_t numIdenticalSymbols = GetNumberIdenticalSymbols(size - 1, j);
+	return (base.find(symbol) != base.end()) ? (base.find(symbol)->second - numIdenticalSymbols) : size;
+}
+
+void RecountPosition(size_t i, std::string &text, Position &position)
+{
+	if (i < text.size())
+	{
+		for (size_t tmp = position.column; tmp <= i; ++tmp)
+		{
+			if (text[tmp] == '\n')
+			{
+				text[tmp] = ' ';
+				++position.row;
+				position.column = tmp + 1;
+			}
+		}
+	}
+}
+
+void OutputPosition(size_t i, const Position &position, std::ofstream &output)
+{
+	(i < position.column) ? output << (position.row - 1) << ' ' << (i + 1) : output << position.row << ' ' << (i - position.column + 1);
+	output << std::endl;
+}
+
+void FindSample(std::string & sample, std::string & text, std::ofstream &output)
 {
 	bool isFind = false;
 	std::multimap <char, size_t> base = GetBase(sample);
+	Position position;
 
-	for (size_t i = sample.size() - 1, j = i, row = 1, num = 0; i < text.size();)
+	for (size_t i = sample.size() - 1, sampleIndex = i; i < text.size();)
 	{
 		ToLowerCase(text[i]);
-
-		if (text[i] == sample[j])
+		if (text[i] == sample[sampleIndex])
 		{
-			if (j == 0)
+			if (sampleIndex == 0)
 			{
+				OutputPosition(i, position, output);
 				isFind = true;
-				j = sample.size() - 1;
-				(i < num) ? output << (row - 1) << ' ' << (i + 1) : output << row << ' ' << (i - num + 1);
-				output << std::endl;
-
+				sampleIndex = sample.size() - 1;
 				i += sample.size();
 			}
 			else
 			{
-				--j;
+				--sampleIndex;
 				--i;
 			}
 		}
 		else
 		{
-			size_t tmp = num;
-			size_t k = 0;
-			if (j < (sample.size() - 1))
-			{
-
-				k = sample.size() - 1 - j;
-				j = sample.size() - 1;
-
-			}
-
-			i += (base.find(text[i]) != base.end()) ? (base.find(text[i])->second - k) : (sample.size());
-
-			if (i < text.size())
-			{
-				for (; tmp <= i; ++tmp)
-				{
-					if (text[tmp] == '\n')
-					{
-						text[tmp] = ' ';
-						++row;
-						num = tmp + 1;
-					}
-				}
-			}
+			i += GetNewIndex(sampleIndex, sample.size(), base, text[i]);
+			RecountPosition(i, text, position);
 		}
 	}
-	return isFind;
+
+	if (!isFind)
+	{
+		output << "No" << std::endl;
+	}
 }
-
-
 
 int main()
 {
@@ -101,11 +124,7 @@ int main()
 	std::string text;
 	getline(dictionary, text, '\0');
 
-	if (!isFindString(sample, text, output))
-	{
-		output << "No" << std::endl;
-	}
+	FindSample(sample, text, output);
 
 	return EXIT_SUCCESS;
 }
-
