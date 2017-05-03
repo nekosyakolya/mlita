a57f494f9806fd6dcfac6c2c7e472c4b21eacdd6
nekosyakolya <greirex@mail.ru>
2017-05-04 00:10:04 +0300

    создание структуры template

--- labs/lw-13/lw-13/lw-13.cpp
+++ labs/lw-13/lw-13/lw-13.cpp
@@ -8,7 +8,7 @@
 
 void ToLowerCase(char & symbol)
 {
-	if ((symbol <= 'Я' && symbol >= 'А') || (symbol <= 'Z' && symbol >= 'A' ))
+	if ((symbol <= 'Я' && symbol >= 'А') || (symbol <= 'Z' && symbol >= 'A'))
 	{
 		symbol += 32;
 	}
@@ -18,45 +18,93 @@ void ToLowerCase(char & symbol)
 	}
 }
 
-std::multimap <char, size_t> GetBase(std::string & sample)
+struct Template
 {
-	std::multimap <char, size_t> tmp;
-	for (int i = sample.size() - 1, j = 0; i >= 0; --i, ++j)
+	std::string string;
+	std::multimap <char, size_t> base;
+	std::vector<size_t>sufficsShift;
+	void GetBase()
 	{
-		if (tmp.find(sample[i]) == tmp.end())
+		std::multimap <char, size_t> tmp;
+		for (int i = string.size() - 1, j = 0; i >= 0; --i, ++j)
 		{
-			ToLowerCase(sample[i]);
-			tmp.emplace(sample[i], static_cast<size_t>(j));
+			if (tmp.find(string[i]) == tmp.end())
+			{
+				ToLowerCase(string[i]);
+				tmp.emplace(string[i], static_cast<size_t>(j));
+			}
 		}
+		std::swap(tmp, base);
 	}
-	return tmp;
-}
 
-size_t GetNumberIdenticalSymbols(size_t lastIndex, size_t &j)
-{
-	size_t result = 0;
-	if (j < lastIndex)
+	void GetShift()
 	{
-		result = lastIndex - j;
-		j = lastIndex;
+		const size_t sampleSize = string.size();
+		std::vector<size_t> tmp(sampleSize + 1, sampleSize);
+		std::vector<size_t> z = GetZFunction();
+
+		for (size_t i = sampleSize - 1; i > 0; --i)
+		{
+			tmp[sampleSize - z[i]] = i;
+		}
+
+		for (size_t i = 1, j = 0; i <= sampleSize - 1; ++i)
+		{
+			if (i + z[i] == sampleSize)
+			{
+				for (; j <= i; ++j)
+				{
+					if (tmp[j] == sampleSize)
+					{
+						tmp[j] = i;
+					}
+				}
+			}
+		}
+		std::swap(tmp, sufficsShift);
 	}
-	return result;
-}
+private:
+	std::vector<size_t> GetZFunction()
+	{
+		const size_t sampleSize = string.size();
+		std::vector<size_t> z(sampleSize, 0);
 
-size_t GetNewIndex(size_t &j, size_t size, const std::multimap <char, size_t>& base, char symbol)
+		for (size_t i = 1, maxZidx = 0, maxZ = 0; i < sampleSize; ++i)
+		{
+			if (i <= maxZ)
+			{
+				z[i] = std::min(maxZ - i + 1, z[i - maxZidx]);
+			}
+
+			while (i + z[i] < sampleSize && string[sampleSize - 1 - z[i]] == string[sampleSize - 1 - (i + z[i])])
+			{
+				++z[i];
+			}
+
+			if (i + z[i] - 1 > maxZ)
+			{
+				maxZidx = i;
+				maxZ = i + z[i] - 1;
+			}
+		}
+
+		return z;
+	}
+};
+
+size_t GetShift(const char &symbol, const std::multimap <char, size_t> &base, size_t sizeSample)
 {
-	size_t numIdenticalSymbols = GetNumberIdenticalSymbols(size - 1, j);
-	return (base.find(symbol) != base.end()) ? (base.find(symbol)->second - numIdenticalSymbols) : size;
+	return (base.find(symbol) != base.end()) ? (base.find(symbol)->second) : sizeSample;
 }
 
-size_t Find(const std::vector<size_t> &positions, const std::string & sample, const std::string & text, size_t i, const std::multimap <char, size_t>& base, const std::vector<int>& suffics)
+size_t Find(const std::vector<size_t> &positions, const Template & sample, const std::string & text, size_t i)
 {
 	size_t result = 1;
 	bool isFind = true;
 	std::string textStr = text;
 	size_t index = positions.size() - 1;
 
-	for (int j = sample.size() - 1; j >= 0;--j, --i)
+	for (int j = sample.string.size() - 1; j >= 0;--j, --i)
 	{
 		if (textStr[i] == '\n')
 		{
@@ -64,111 +112,63 @@ size_t Find(const std::vector<size_t> &positions, const std::string & sample, co
 			--index;
 		}
 
-		if (sample[j] != textStr[i])
+		ToLowerCase(textStr[i]);
+		if (sample.string[j] != textStr[i])
 		{
 			isFind = false;
-			result = (base.find(textStr[i]) != base.end()) ? (base.find(textStr[i])->second) : sample.size();
-			if (result < ((sample.size() - 1) - j))
-			{
-				result = suffics[j];// + 1 мб...
-			}
-			else
-			{
-				result -= ((sample.size() - 1) - j);
-			}
-
+			result = GetShift(textStr[i], sample.base, sample.string.size());
+			result = (result < ((sample.string.size() - 1) - j)) ? sample.sufficsShift[j] : (result - ((sample.string.size() - 1) - j));
 			break;
 		}
 	}
-	
-
 	if (isFind)
 	{
-		std::cout << "row: "  << (index + 1) << " col:" << ((i - positions[index]) + 2) << std::endl;
+		std::cout << (index + 1) << ' ' <<((i - positions[index]) + 2) << std::endl;
 	}
-
 	return result;
 }
 
-std::vector<int> GetShift(const std::string & sample)
+void FindSampleInText(Template &sample, std::ifstream &dictionary, std::ofstream &output)
 {
-	size_t m = sample.size();
-
-	std::vector<int> suffshift(m + 1, m);
-	std::vector<int> z(m, 0);
-	for (int j = 1, maxZidx = 0, maxZ = 0; j < m; ++j) {
-		if (j <= maxZ) z[j] = std::min(maxZ - j + 1, z[j - maxZidx]);
-		while (j + z[j] < m && sample[m - 1 - z[j]] == sample[m - 1 - (j + z[j])]) z[j]++;
-		if (j + z[j] - 1 > maxZ) {
-			maxZidx = j;
-			maxZ = j + z[j] - 1;
-		}
-	}
-	for (int j = m - 1; j > 0; j--) suffshift[m - z[j]] = j; //цикл №1
-	for (int j = 1, r = 0; j <= m - 1; j++) //цикл №2
-		if (j + z[j] == m)
-			for (; r <= j; r++)
-				if (suffshift[r] == m) suffshift[r] = j;
-
-
-	return suffshift;
-}
-
-
-int main()
-{
-	std::ifstream input("input.txt");
-	std::ofstream output("output.txt");
-
-	std::string sample;
-	std::string fileName;
-
-	getline(input, sample);
-	getline(input, fileName);
-
-	std::ifstream dictionary(fileName);
 	std::string text;
-
 	getline(dictionary, text);
+	std::vector<size_t> positions = { 0 };
 
-	std::multimap <char, size_t> base = GetBase(sample);
-
-	std::vector<int>result = GetShift(sample);
-
-	std::vector<size_t> positions;
-	positions.push_back(0);
-
-	for (size_t i = sample.size() - 1; i < text.size() || !dictionary.eof();)
+	for (size_t i = sample.string.size() - 1; i < text.size() || !dictionary.eof();)
 	{
-
 		if (i >= text.size() && !dictionary.eof())
 		{
 			std::string tmp;
 			getline(dictionary, tmp);
 			text += '\n';
-
-			positions.push_back (text.size());
-			
+			positions.push_back(text.size());
 			text += tmp;
 		}
 		else
 		{
 			ToLowerCase(text[i]);
-
-
-
-			if (text[i] == sample.back())
-			{
-				i += Find(positions, sample, text, i, base, result);
-			}
-			else
-			{
-				char symbol = (text[i] == '\n') ? ' ' : text[i];
-				
-				i += (base.find(symbol) != base.end()) ? (base.find(symbol)->second) : sample.size();
-			}
+			char symbol = (text[i] == '\n') ? ' ' : text[i];
+			i += (symbol == sample.string.back()) ? Find(positions, sample, text, i) : GetShift(symbol, sample.base, sample.string.size());
 		}
 	}
+}
 
+int main()
+{
+	std::ifstream input("input.txt");
+	std::ofstream output("output.txt");
+	
+	Template sample;
+	std::string fileName;
+
+	getline(input, sample.string);
+	getline(input, fileName);
+
+	std::ifstream dictionary(fileName);
+
+	sample.GetBase();
+	sample.GetShift();
+
+	FindSampleInText(sample, dictionary, output);
 	return EXIT_SUCCESS;
 }
